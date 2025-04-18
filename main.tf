23ac3d81df2172e2971fb47e534edddf13ef5b94
SameerBramhecha <bramhechas89@gmail.com>
2025-04-18 22:24:07 +0530

    added basic config terraform code for storage account

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 module "resource-group"{
     source = "./resource-group"
 }
+
+module "storage-account"{
+    source = "./storage-account"
+    location = module.resource-group.location
+    resource_group_name     = module.resource-group.name
+}
---
+++ storage-account/variables.tf
@@ -0,0 +1,9 @@
+variable "storage_account_name" {
+    description = "The name of the storage account to create."
+    type        = string
+    default     = "mystorageaccountsameerb"
+}
+
+variable "resource_group_name" {}
+
+variable "location" {}
